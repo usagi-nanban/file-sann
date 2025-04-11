5cc379dd3dbd315be04b4380b7efca3b736dd64c
usagi-nanban <so.yone2011@gmail.com>
2025-04-11 17:45:21 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyClRN44ehnNYXuJ_P3g8ujcp-Xsl9-SecM",
+  authDomain: "new-file-3e4db.firebaseapp.com",
+  databaseURL: "https://new-file-3e4db-default-rtdb.firebaseio.com",
+  projectId: "new-file-3e4db",
+  storageBucket: "new-file-3e4db.appspot.com",
+  messagingSenderId: "485447051421",
+  appId: "1:485447051421:web:82bb9866ff38d80a540c74",
+  measurementId: "G-W4B14F8RWG"
+};
